c5559a9d872593a7ef7c58b763230f6289c6ae8a
einharan <taufan.augusta@protonmail.com>
2021-12-11 20:32:24 +0700

    test 2

--- main.cpp
+++ main.cpp
@@ -2,22 +2,20 @@
 #include <string>
 using namespace std;
 
-void Solution(bool parameter)
-{
-    string msg = "solution";
-    string s = parameter == true ? "TRUE" : "FALSE";
-    cout << msg << ": " << s << endl;
-}
 
 int main()
 {
-    bool x = true;
-    bool y = false;
-    bool z = true;
-    bool w = !y;
+    int n = 5;
+    int k = 2 * n - 2;
 
-    Solution(w);
-
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            cout << j <<" ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
