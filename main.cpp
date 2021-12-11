ca14f9d84c94202fec7b7cec562703564b52fc70
einharan <taufan.augusta@protonmail.com>
2021-12-11 21:21:56 +0700

    diamond

--- main.cpp
+++ main.cpp
@@ -5,17 +5,42 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
-    int k = 2 * n - 2;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < k; j++)
+    int n=9;
+    int tengah= n/2;
+    int batasKiri=n/2;
+    int batasKanan=n/2;
+    
+    
+    for(int i=0; i<n; i++){
+        cout<< "L:" << batasKiri << " ";
+        
+        for(int j=0; j<n; j++)
         {
-            cout << j <<" ";
+            if(j==batasKiri) // jika J itu bernilai = batas kiri -> cetak ■
+            {
+                cout<<"■"<<" ";
+            }
+            else if(j==batasKanan) // jika bernilai = batas kanan JUGA -> cetak ■
+            {
+                cout<<"■"<<" ";
+            }
+            else // jika kedua kondisi tidak terpenuhi lakukan yg dibawah
+            {
+                cout<< "_" <<" ";
+
+            }
+            
         }
-        cout << endl;
+        if(batasKiri<n-1 && i>=n/2){
+            batasKiri++;
+            batasKanan--;
+        }else{
+            batasKiri--;
+            batasKanan++;
+        }
+
+        cout<< "R:" << batasKanan-1 << " ";
+        cout<<endl;
     }
-    return 0;
 }
 
